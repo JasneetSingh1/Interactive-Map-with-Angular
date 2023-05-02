de61e953254a1c308f8d2d7c2b16eb3a66693765
Jasneet <jasneetpalsingh619@gmail.com>
2023-05-02 17:46:15 -0600

    added html for map details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { MapMapComponent } from './map/map-map/map-map.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'interactive-map-angular';
+
+  constructor(){
+    window.addEventListener("load", function(){
+      // const infoContainer = document.getElementById('info-con');
+      // const data = await fetch("http://api.worldbank.org/v2/country/br?format=json")
+      // const countryData = await data.json();
+     
+      
+      var svg = document.getElementById('ad');
+      console.log(svg)
+      var cName = svg?.nodeValue
+      console.log(cName)
+      })
+  }
+ 
 }
